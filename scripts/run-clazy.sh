38358525dc8f26375f268461b3391d5daf3792b6
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2020-03-26 18:08:29 +0200

    Make sure not to run clazy in a build directory since it will show a lot of junk

--- scripts/run-clazy.sh
+++ scripts/run-clazy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd src/
+
 find . \( -name '*.hpp' \) -exec clazy-standalone -checks=level1 -p ./compile_commands.json {} \;
 
 find . \( -name '*.cpp' \) -exec clazy-standalone -checks=level1 -p ./compile_commands.json {} \;
