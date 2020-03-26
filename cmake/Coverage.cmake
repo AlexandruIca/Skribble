2629f20791cc2309ed63df847e51a526eed137bc
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2020-03-26 19:25:45 +0200

    Add option for getting code coverage

---
+++ cmake/Coverage.cmake
@@ -0,0 +1,12 @@
+function(enable_coverage project_name)
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" OR CMAKE_CXX_COMPILER_ID STREQUAL
+                                             "Clang")
+    option(ENABLE_COVERAGE "Enable code coverage reporting for gcc/clang."
+           FALSE)
+
+    if(ENABLE_COVERAGE)
+      target_compile_options(${project_name} INTERFACE --coverage -O0 -g)
+      target_link_libraries(${project_name} INTERFACE --coverage)
+    endif()
+  endif()
+endfunction()
