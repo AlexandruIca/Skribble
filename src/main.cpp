72d41b8dbf6098d40f945884cc5b07c69794f201
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2020-03-26 15:37:40 +0200

    Add basic loader for QML

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello world!" << std::endl;
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app{ argc, argv };
+    QQmlApplicationEngine engine{};
+
+    engine.load(QUrl{ QStringLiteral("qrc:/main.qml") });
+
+    return app.exec();
 }
