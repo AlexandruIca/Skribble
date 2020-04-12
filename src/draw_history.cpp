0d7eff9f7f1e2f6ea80fe9b8d9e1dd78acb57428
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2020-04-12 09:28:14 +0300

    Separate draw_history in .hpp and .cpp file

---
+++ src/draw_history.cpp
@@ -0,0 +1,148 @@
+#include "draw_history.hpp"
+
+namespace sk::impl {
+
+auto CachedLayers::PixmapDrawer(QPixmap& dest, QPixmap& src) -> void
+{
+    QPainter painter{ &dest };
+    painter.drawPixmap(
+        CachedLayers::m_canvasRect, src, CachedLayers::m_canvasRect);
+}
+
+CachedLayers::CachedLayers(bool const foreign)
+    : m_foreign{ foreign }
+{
+    m_layers.emplaceBack(sk::config::width, sk::config::height)
+        .fill(m_transparent);
+}
+
+auto CachedLayers::pushNewLayer() -> void
+{
+    m_layers.emplaceBack(sk::config::width, sk::config::height)
+        .fill(m_transparent);
+}
+
+auto CachedLayers::paintBlock(QPainter& painter) -> void
+{
+    m_layers.reduceTo([&painter](QPixmap const& src) -> void {
+        painter.drawPixmap(m_canvasRect, src, m_canvasRect);
+    });
+}
+
+[[nodiscard]] auto CachedLayers::getLastLayer() noexcept -> QPixmap&
+{
+    return m_layers.getLast();
+}
+
+[[nodiscard]] auto CachedLayers::getLastLayer() const noexcept -> QPixmap const&
+{
+    return m_layers.getLast();
+}
+
+[[nodiscard]] auto CachedLayers::undo() -> bool
+{
+    return m_layers.undo();
+}
+
+auto CachedLayers::redo() -> bool
+{
+    return m_layers.redo();
+}
+
+} // namespace sk::impl
+
+namespace sk {
+
+auto DrawHistory::CachedDrawer(impl::CachedLayers& dest,
+                               impl::CachedLayers& src) -> void
+{
+    QPainter painter{ &dest.getLastLayer() };
+    src.paintBlock(painter);
+}
+
+[[nodiscard]] auto DrawHistory::getLastLayer(bool const foreign) -> QPixmap&
+{
+    return this->getLastLayerIter(foreign).getLastLayer();
+}
+
+[[nodiscard]] auto DrawHistory::getLastLayerIter(bool const foreign)
+    -> impl::CachedLayers&
+{
+    auto it = m_layers.getUnderlying().rbegin();
+
+    while(it != m_layers.getUnderlying().rend() && it->foreign() != foreign) {
+        ++it;
+    }
+
+    if(it->foreign() != foreign) {
+        // ERROR
+    }
+
+    return *it;
+}
+
+DrawHistory::DrawHistory()
+{
+    m_layers.emplaceBack();
+}
+
+auto DrawHistory::pushNewLayer(bool const foreign) -> void
+{
+    if(m_layers.getUnderlying().back().foreign() != foreign) {
+        m_layers.emplaceBack(foreign);
+    }
+    else {
+        m_layers.getUnderlying().back().pushNewLayer();
+    }
+    m_lastPoint = std::nullopt;
+}
+
+auto DrawHistory::paintCanvas(QPainter* const painter) -> void
+{
+    m_layers.reduceTo([&painter](impl::CachedLayers& src) -> void {
+        src.paintBlock(*painter);
+    });
+}
+
+auto DrawHistory::drawAt(QPoint const& pos, QPen const& pen, bool const foreign)
+    -> void
+{
+    if(auto& last = this->getLastLayerIter(foreign); last.underUndo()) {
+        last.pushNewLayer();
+    }
+
+    QPainter painter{ &this->getLastLayer(foreign) };
+    painter.setPen(pen);
+
+    if(m_lastPoint.has_value()) {
+        painter.drawLine(m_lastPoint.value(), pos);
+    }
+    else {
+        painter.drawPoint(pos);
+    }
+
+    m_lastPoint = pos;
+}
+
+auto DrawHistory::undo(bool const foreign) -> void
+{
+    // We undo two times here if undo is hit for the 'first' time
+    // because remember: when the user releases the left click
+    // we create a new layer which is an empty layer.
+    // If we delete this if then the user has to press 'Ctrl-z' two times
+    // to see the undo take effect because the firsst time it only skips
+    // the empty layer.
+    auto& last = this->getLastLayerIter(foreign);
+    /*
+    if(!last.underUndo()) {
+        static_cast<void>(last.undo());
+    }*/
+    static_cast<void>(last.undo());
+}
+
+auto DrawHistory::redo(bool const foreign) -> void
+{
+    static_cast<void>(this->getLastLayerIter(foreign).redo());
+}
+
+} // namespace sk
--- src/draw_history.hpp
+++ src/draw_history.hpp
@@ -23,12 +23,7 @@ class CachedLayers
 private:
     bool const m_foreign{ false };
 
-    static auto PixmapDrawer(QPixmap& dest, QPixmap& src) -> void
-    {
-        QPainter painter{ &dest };
-        painter.drawPixmap(
-            CachedLayers::m_canvasRect, src, CachedLayers::m_canvasRect);
-    }
+    static auto PixmapDrawer(QPixmap& dest, QPixmap& src) -> void;
 
     sk::CachedResource<QPixmap> m_layers{ &PixmapDrawer };
 
@@ -38,12 +33,7 @@ private:
     };
 
 public:
-    explicit CachedLayers(bool const foreign = false)
-        : m_foreign{ foreign }
-    {
-        m_layers.emplaceBack(sk::config::width, sk::config::height)
-            .fill(m_transparent);
-    }
+    explicit CachedLayers(bool const foreign = false);
     CachedLayers(CachedLayers const&) = default;
     CachedLayers(CachedLayers&&) = default;
     ~CachedLayers() noexcept = default;
@@ -51,27 +41,10 @@ public:
     auto operator=(CachedLayers const&) -> CachedLayers& = delete;
     auto operator=(CachedLayers &&) -> CachedLayers& = delete;
 
-    auto pushNewLayer() -> void
-    {
-        m_layers.emplaceBack(sk::config::width, sk::config::height)
-            .fill(m_transparent);
-    }
-
-    auto paintBlock(QPainter& painter) -> void
-    {
-        m_layers.reduceTo([&painter](QPixmap const& src) -> void {
-            painter.drawPixmap(m_canvasRect, src, m_canvasRect);
-        });
-    }
-
-    [[nodiscard]] auto getLastLayer() noexcept -> QPixmap&
-    {
-        return m_layers.getLast();
-    }
-    [[nodiscard]] auto getLastLayer() const noexcept -> QPixmap const&
-    {
-        return m_layers.getLast();
-    }
+    auto pushNewLayer() -> void;
+    auto paintBlock(QPainter& painter) -> void;
+    [[nodiscard]] auto getLastLayer() noexcept -> QPixmap&;
+    [[nodiscard]] auto getLastLayer() const noexcept -> QPixmap const&;
 
     [[nodiscard]] constexpr auto foreign() const noexcept -> bool
     {
@@ -87,15 +60,8 @@ public:
     /// \returns true If undo was successful.
     ///          false If already at oldest change
     ///
-    [[nodiscard]] auto undo() -> bool
-    {
-        return m_layers.undo();
-    }
-
-    auto redo() -> bool
-    {
-        return m_layers.redo();
-    }
+    [[nodiscard]] auto undo() -> bool;
+    auto redo() -> bool;
 };
 
 } // namespace sk::impl
@@ -106,11 +72,7 @@ class DrawHistory
 {
 private:
     static auto CachedDrawer(impl::CachedLayers& dest, impl::CachedLayers& src)
-        -> void
-    {
-        QPainter painter{ &dest.getLastLayer() };
-        src.paintBlock(painter);
-    }
+        -> void;
 
     struct Traits
     {
@@ -122,33 +84,12 @@ private:
     sk::CachedResource<impl::CachedLayers, Traits> m_layers{ &CachedDrawer };
     std::optional<QPoint> m_lastPoint{ std::nullopt };
 
-    [[nodiscard]] auto getLastLayer(bool const foreign = false) -> QPixmap&
-    {
-        return this->getLastLayerIter(foreign).getLastLayer();
-    }
-
+    [[nodiscard]] auto getLastLayer(bool const foreign = false) -> QPixmap&;
     [[nodiscard]] auto getLastLayerIter(bool const foreign = false)
-        -> impl::CachedLayers&
-    {
-        auto it = m_layers.getUnderlying().rbegin();
-
-        while(it != m_layers.getUnderlying().rend() &&
-              it->foreign() != foreign) {
-            ++it;
-        }
-
-        if(it->foreign() != foreign) {
-            // ERROR
-        }
-
-        return *it;
-    }
+        -> impl::CachedLayers&;
 
 public:
-    DrawHistory()
-    {
-        m_layers.emplaceBack();
-    }
+    DrawHistory();
     DrawHistory(DrawHistory const&) = default;
     DrawHistory(DrawHistory&&) = default;
     ~DrawHistory() noexcept = default;
@@ -156,64 +97,13 @@ public:
     auto operator=(DrawHistory const&) -> DrawHistory& = default;
     auto operator=(DrawHistory &&) -> DrawHistory& = default;
 
-    auto pushNewLayer(bool const foreign = false) -> void
-    {
-        if(m_layers.getUnderlying().back().foreign() != foreign) {
-            m_layers.emplaceBack(foreign);
-        }
-        else {
-            m_layers.getUnderlying().back().pushNewLayer();
-        }
-        m_lastPoint = std::nullopt;
-    }
-
-    auto paintCanvas(QPainter* const painter) -> void
-    {
-        m_layers.reduceTo([&painter](impl::CachedLayers& src) -> void {
-            src.paintBlock(*painter);
-        });
-    }
+    auto pushNewLayer(bool const foreign = false) -> void;
+    auto paintCanvas(QPainter* const painter) -> void;
 
     auto drawAt(QPoint const& pos, QPen const& pen, bool const foreign = false)
-        -> void
-    {
-        if(auto& last = this->getLastLayerIter(foreign); last.underUndo()) {
-            last.pushNewLayer();
-        }
-
-        QPainter painter{ &this->getLastLayer(foreign) };
-        painter.setPen(pen);
-
-        if(m_lastPoint.has_value()) {
-            painter.drawLine(m_lastPoint.value(), pos);
-        }
-        else {
-            painter.drawPoint(pos);
-        }
-
-        m_lastPoint = pos;
-    }
-
-    auto undo(bool const foreign = false) -> void
-    {
-        // We undo two times here if undo is hit for the 'first' time
-        // because remember: when the user releases the left click
-        // we create a new layer which is an empty layer.
-        // If we delete this if then the user has to press 'Ctrl-z' two times
-        // to see the undo take effect because the firsst time it only skips
-        // the empty layer.
-        auto& last = this->getLastLayerIter(foreign);
-        /*
-        if(!last.underUndo()) {
-            static_cast<void>(last.undo());
-        }*/
-        static_cast<void>(last.undo());
-    }
-
-    auto redo(bool const foreign = false) -> void
-    {
-        static_cast<void>(this->getLastLayerIter(foreign).redo());
-    }
+        -> void;
+    auto undo(bool const foreign = false) -> void;
+    auto redo(bool const foreign = false) -> void;
 };
 
 } // namespace sk
