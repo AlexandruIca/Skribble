7cbd118aef7f47d01c4b344583f583bbb4f47d32
Alexandru Ica Gabriel <alexandruica703@gmail.com>
2020-04-13 18:49:50 +0300

    Add test for emplaceBack

--- tests/cached_resource_test.cpp
+++ tests/cached_resource_test.cpp
@@ -89,3 +89,60 @@ TEST("[CachedResource] Undo/Redo")
     ASSERT(redo == false);
     ASSERT(res.underUndo() == false);
 }
+
+template<typename T>
+struct Trait2
+{
+    using ContainerType = std::deque<T>;
+    static constexpr int cacheGap = 5;
+    static constexpr int maxCount = sk::ResourceTraits<T>::maxCount;
+};
+
+TEST("[CachedResource] emplaceBack")
+{
+    sk::CachedResource<int, Trait2<int>> res{ adder };
+    bool undo{ true };
+    bool redo{ true };
+
+    for(int i = 0; i < Trait2<int>::cacheGap * 2; ++i) {
+        res.emplaceBack(i + 1);
+        ASSERT(res.underUndo() == false);
+    }
+
+    ASSERT(res.getLast() == 10);
+    ASSERT(*(res.getLastCache()) == 15);
+
+    for(int i = 0; i < Trait2<int>::cacheGap; ++i) {
+        undo = res.undo();
+        ASSERT(undo == true);
+        ASSERT(res.underUndo() == true);
+    }
+
+    ASSERT(res.getLastCache() == nullptr);
+    ASSERT(res.getLast() == 5);
+
+    undo = res.undo();
+
+    ASSERT(undo == true);
+    ASSERT(res.getLastCache() == nullptr);
+
+    redo = res.redo();
+
+    ASSERT(redo == true);
+    ASSERT(res.getLastCache() != nullptr);
+    ASSERT(*(res.getLastCache()) == 15);
+    ASSERT(res.underUndo() == true);
+
+    res.emplaceBack(100);
+
+    ASSERT(res.underUndo() == false);
+    ASSERT(res.redo() == false);
+    ASSERT(res.getLastCache() != nullptr);
+    ASSERT(*(res.getLastCache()) == 15);
+    ASSERT(res.getLast() == 100);
+
+    int sum{ 0 };
+    res.reduceTo(sum);
+
+    ASSERT(sum == 115);
+}
