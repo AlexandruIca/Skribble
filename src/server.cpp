d2ff3657fa2f0bb380137df47a0875853b2b32cf
Alexandru Ica <alexandruica703@gmail.com>
2020-05-09 17:11:23 +0300

    Fix styling

--- src/server.cpp
+++ src/server.cpp
@@ -13,11 +13,9 @@ Server::Server()
 
     connect(m_server.get(), &QTcpServer::newConnection, [this] {
         m_socket = m_server->nextPendingConnection();
-        // std::make_unique<QTcpSocket>(m_server->nextPendingConnection());
 
         if(m_socket == nullptr) {
-            qDebug()
-                << "[Server] Could't create socket for pending connecti    on";
+            qDebug() << "[Server] Could't create socket for pending connection";
         }
 
         m_socket->write("Hello from server");
